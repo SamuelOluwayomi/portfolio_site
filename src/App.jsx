b17a0876cec19c60a643f5dd1c52f9d89d43b03a
Samuel Oluwayomi <samuelfaith500@gmail.com>
2025-10-18 11:04:13 +0100

    Adding vercel web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import { useEffect } from 'react';
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="min-h-screen bg-brand-mauve">
       <SpeedInsights/>
+      <Analytics/>
       <Navbar />
       <Hero />
       <About />
